825d966a7bb56b90c8dce1d38aa24ac458c26c20
YuraDerevenko <jedprankster7@gmail.com>
2015-09-25 09:01:49 +0300

    This commit contains server, which we use to create database tables and extentions.

--- constants/tables.js
+++ constants/tables.js
@@ -1,13 +1,13 @@
 module.exports = {
-    TEST_TABLE: 'test',
     HOSPITALS: 'tb_hospitals',
-    SUB_TREATMENTS: 'tb_hospiital_sub_treatments',
-    TREATMENTS: 'tb_hospiital_treatments',
+    SUB_TREATMENTS: 'tb_hospital_sub_treatments',
+    TREATMENTS: 'tb_hospital_treatments',
     HOSPITAL_TEXTS: 'tb_hospital_texts',
     HOSPITAL_TYPES_LIST: 'tb_hospital_types_dic',
     IMAGES: 'tb_images',
-    REGIONS_LIST: 'tb_regions dic',
-    SUB_TREATMENTS_LIST: 'tb_sub_treatments dic',
-    TREATMENTS_LIST: 'tb_treatments dic',
-    USERS: 'tb_users'
+    REGIONS_LIST: 'tb_regions_dic',
+    SUB_TREATMENTS_LIST: 'tb_sub_treatments_dic',
+    TREATMENTS_LIST: 'tb_treatments_dic',
+    USERS: 'tb_users',
+    NEWS: 'tb_news'
 };
