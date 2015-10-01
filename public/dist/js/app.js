4859a4ba73373870fb041332ccbb9c4f2f55d57b
Alejandro75157 <gravidor@gmail.com>
2015-10-01 17:54:06 +0300

    Added basic sentere view

--- public/dist/js/app.js
+++ public/dist/js/app.js
@@ -38882,6 +38882,10 @@ app.config(['$routeProvider', function ($routeProvider) {
         controller: 'sentereController',
         templateUrl: 'templates/sentere/list.html',
         controllerAs: 'sentereCtrl'
+    }).when('/sentere/:id', {
+        controller: 'hospitalController',
+        templateUrl: 'templates/sentere/view.html',
+        controllerAs: 'hospitalCtrl'
     }).otherwise({
         redirectTo: '/'
     });
@@ -38891,6 +38895,31 @@ app.config(['$routeProvider', function ($routeProvider) {
 
 }]);
 ;
+app.controller('hospitalController', ['$scope', 'GeneralHelpers',
+    function ($scope, GeneralHelpers) {
+        var self = this;
+
+        //$scope.itemsPerPage = GeneralHelpers.getLocalData('resultater');
+
+        this.hospital = {
+            image: 'http://www.freelargeimages.com/wp-content/uploads/2015/05/Hospital_Logo_02.png',
+            title: 'New Hospital',
+            created_at: new Date(),
+            phone: '+9379992',
+            details: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sed tortor dolor. Nullam eu aliquam ' +
+            'libero. In ac ultrices odio, eu imperdiet arcu. Nullam sagittis consectetur orci. Aliquam lacinia nisi eu ' +
+            'vestibulum suscipit. Proin finibus leo ac dapibus ornare. Pellentesque nisi massa, scelerisque sit amet ' +
+            'quam nec, porta lobortis odio. Aliquam ullamcorper, nibh varius fringilla molestie, felis lorem aliquam ' +
+            'arcu, eget posuere erat leo id leo. Nam pulvinar mauris vulputate magna mollis ornare. Quisque vitae ' +
+            'varius justo, ut rhoncus velit.',
+            address: '93 Cottonwood Drive  North York' +
+            ' ON M3C 2B3 Canada',
+            is_paid: true,
+            email: 'new@hosp.com',
+            latitude: 32,
+            longitude: 23
+        };
+    }]);;
 app.controller('sentereController', ['$scope', 'GeneralHelpers',
     function ($scope, GeneralHelpers) {
     var self = this;
