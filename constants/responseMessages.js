a97b1d307c8eef15893e3f740cf268b08dd4e1e6
Alejandro75157 <gravidor@gmail.com>
2015-10-27 11:41:34 +0200

    Added regions db import from outer source, added get fylkes, update regions db and get hospitals with conflicts,

--- constants/responseMessages.js
+++ constants/responseMessages.js
@@ -35,6 +35,7 @@ module.exports = {
     PASSWORD_TOO_SHORT: 'Password should be at least 6 characters long',
     PASSWORD_NOT_EQUAL: 'Password is not equal',
     INCORRECT_LOGIN_PASS: 'Your log in details are incorrect, please try again',
+    UPDATED_REGIONS_DB_SUCCESS: 'Regions DB Was Successfully Updated',
 
     USER_USED_FIELDS: {
         EMAIL: 'Email is already used',
