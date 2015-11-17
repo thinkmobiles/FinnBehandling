424107f18b7ab5525fb4651f04df6035cc47f37e
Roma <roma.buchuk@gmail.com>
2015-11-17 11:23:00 +0000

    added server config

--- config/development.js
+++ config/development.js
@@ -1,5 +1,5 @@
-process.env.HOST = 'http://localhost:8787/';
-process.env.APP_HOST = 'http://localhost';
+//process.env.APP_HOST = 'http://projects.thinkmobiles.com';
+process.env.HOST = 'http://projects.thinkmobiles.com:8787/';
 process.env.PORT = 8787;
 process.env.DB_HOST = 'localhost';
 //process.env.DB_HOST = '192.168.88.250';
@@ -14,5 +14,5 @@ process.env.LOCAL_IMAGE_STORAGE = 'public';
 
 process.env.REDIS_HOST = 'localhost';
 process.env.REDIS_PORT = 6379;
-process.env.REDIS_DB_KEY = 8;
+process.env.REDIS_DB_KEY = 17;
 
