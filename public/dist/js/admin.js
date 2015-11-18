5acffa3407b673619534ad6d8e1e40c4ee870b50
Vasyl Hoshovsky <vasyl.hoshovsky@gmail.com>
2015-11-18 17:32:43 +0200

    Hospital list async pages loading implemented

--- public/dist/js/admin.js
+++ public/dist/js/admin.js
@@ -73702,20 +73702,20 @@ app.controller('listHospitalController', ['$scope', 'HospitalsManager', 'Regions
         vm.searchText = '';
 
         vm.currentPage = 1;
-        vm.resultCount = 5;
+        vm.resultCount = 25;
+        vm.resultTotal = 0;
         vm.resultCountVariants = [5, 10, 25, 50, 100, 200];
 
         vm.hospitals = [];
 
         vm.search = search;
         vm.deleteHospital = deleteHospital;
-
+        vm.pageChanged = pageChanged;
 
         search();
 
         RegionsManager.getFylkes(function (err, data) {
             if (!err) {
-                //console.log(data);
                 vm.fylkes = data;
             }
         });
@@ -73726,15 +73726,30 @@ app.controller('listHospitalController', ['$scope', 'HospitalsManager', 'Regions
             }
         });
 
+        function pageChanged(newPageNumber) {
+            vm.currentPage = newPageNumber;
+            search();
+        }
+
         /**
          * GET hospitals with applied filters
          */
         function search() {
-            HospitalsManager.getHospitalsList(getFilters(), function (err, data) {
+            var filters = getFilters();
+
+            HospitalsManager.getHospitalsCount(filters, function(err, data){
+                if (!err) {
+                    vm.resultTotal = parseInt(data.count);
+                }
+            });
+
+            HospitalsManager.getHospitalsList(filters, function (err, data) {
                 if (!err) {
                     vm.hospitals = data;
                 }
             });
+
+
         }
 
         /**
@@ -73744,7 +73759,7 @@ app.controller('listHospitalController', ['$scope', 'HospitalsManager', 'Regions
         function getFilters() {
             var filters = {};
 
-            filters.limit = vm.resultCount + 1;
+            filters.limit = vm.resultCount;
             filters.page = vm.currentPage;
             filters.fylke = vm.chosenFylke;
             filters.textSearch = vm.searchText;
