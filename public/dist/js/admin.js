b05f00e56e81ab8d1e5d580a8075fb58a7a67774
Vasyl Hoshovsky <vasyl.hoshovsky@gmail.com>
2015-11-26 12:01:02 +0200

    Startside overview page completed

--- public/dist/js/admin.js
+++ public/dist/js/admin.js
@@ -74013,6 +74013,21 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
 
         self.showArchiveToggle = showArchiveToggle;
         self.isArchiveShown = isArchiveShown;
+        self.deleteStaticNew = deleteStaticNew;
+        self.toggleLimit = toggleLimit;
+
+
+
+        function toggleLimit (staticNew, limit) {
+            console.log(staticNew)
+            if (staticNew.limit === limit) {
+                staticNew.limit = staticNew.content.length;
+                staticNew.showAll = true;
+            } else {
+                staticNew.limit = limit;
+                staticNew.showAll = false;
+            }
+        }
 
         /**
          * In case of staticNewsArchive is empty get data from db and show list of static news for specified branch
@@ -74045,11 +74060,13 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
          * @param staticNewId
          */
         function deleteStaticNew(staticNewId) {
-            StartSideManager.deleteStaticNew(staticNewId, function (err, response) {
-                if (err) {
-                    return GeneralHelpers.showErrorMessage({message: err.data.error, status: err.status});
-                }
-            });
+            if (confirm('Are you sure?')) {
+                StartSideManager.deleteStaticNew(staticNewId, function (err, response) {
+                    if (err) {
+                        return GeneralHelpers.showErrorMessage({message: err.data.error, status: err.status});
+                    }
+                });
+            }
         }
 
         (function getStartSide() {
@@ -74080,6 +74097,11 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
                 }
 
                 self.staticNews = staticNews;
+
+                for (var i = self.staticNews.length-1; i >= 0; i--) {
+                    self.staticNews[i].limit = 800;
+                    self.staticNews[i].showAll = false;
+                }
             });
         }
 
@@ -74097,6 +74119,11 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
                 }
 
                 self.staticNewsArchive[position] = staticNewsArchive;
+
+                for (var i = self.staticNewsArchive[position].length-1; i >= 0; i--) {
+                    self.staticNewsArchive[position][i].limit = 400;
+                    self.staticNewsArchive[position][i].showAll = false;
+                }
             });
         }
 
--- public/js/admin/controllers/startSide/startSide.js
+++ public/js/admin/controllers/startSide/startSide.js
@@ -7,6 +7,21 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
 
         self.showArchiveToggle = showArchiveToggle;
         self.isArchiveShown = isArchiveShown;
+        self.deleteStaticNew = deleteStaticNew;
+        self.toggleLimit = toggleLimit;
+
+
+
+        function toggleLimit (staticNew, limit) {
+            console.log(staticNew)
+            if (staticNew.limit === limit) {
+                staticNew.limit = staticNew.content.length;
+                staticNew.showAll = true;
+            } else {
+                staticNew.limit = limit;
+                staticNew.showAll = false;
+            }
+        }
 
         /**
          * In case of staticNewsArchive is empty get data from db and show list of static news for specified branch
@@ -39,11 +54,13 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
          * @param staticNewId
          */
         function deleteStaticNew(staticNewId) {
-            StartSideManager.deleteStaticNew(staticNewId, function (err, response) {
-                if (err) {
-                    return GeneralHelpers.showErrorMessage({message: err.data.error, status: err.status});
-                }
-            });
+            if (confirm('Are you sure?')) {
+                StartSideManager.deleteStaticNew(staticNewId, function (err, response) {
+                    if (err) {
+                        return GeneralHelpers.showErrorMessage({message: err.data.error, status: err.status});
+                    }
+                });
+            }
         }
 
         (function getStartSide() {
@@ -74,6 +91,11 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
                 }
 
                 self.staticNews = staticNews;
+
+                for (var i = self.staticNews.length-1; i >= 0; i--) {
+                    self.staticNews[i].limit = 800;
+                    self.staticNews[i].showAll = false;
+                }
             });
         }
 
@@ -91,6 +113,11 @@ app.controller('startSideController', ['$scope', 'StartSideManager', 'NewsManage
                 }
 
                 self.staticNewsArchive[position] = staticNewsArchive;
+
+                for (var i = self.staticNewsArchive[position].length-1; i >= 0; i--) {
+                    self.staticNewsArchive[position][i].limit = 400;
+                    self.staticNewsArchive[position][i].showAll = false;
+                }
             });
         }
 
